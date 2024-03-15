1aab2ce6a870e72251bc856b02aa9f67646fb5e5
hungzin311 <hung123ka5@gmail.com>
2024-03-15 22:26:28 +0700

    Complete 6.3

---
+++ Week1/Star/src/Star.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Star {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = keyboard.nextInt();
+        String star = "*";
+        for(int i=1; i <=n; i++){
+            String empty = "";
+            for(int j = 0; j< (n-i); j++) empty += ' '; 
+            System.out.println(empty + star + empty);
+            star += "**";
+        }
+        
+    }
+}
