2b638696bfda3a85651c380964388f45f1d6cc3f
hungzin311 <hung123ka5@gmail.com>
2024-03-15 23:22:54 +0700

    Complete 2.2.6

--- Week1/Equation.java
+++ Week1/Equation.java
@@ -1,6 +1,6 @@
 import javax.swing.JOptionPane;
 public class Equation{
-    public static void main(String[] args) {
+    public static void firstoder_onevar() {
 
         // first order equation with one variable
         String Strnum1, Strnum2; 
@@ -16,7 +16,8 @@ public class Equation{
             double res = -num2/num1;
             JOptionPane.showMessageDialog(null, res, "Root of first order equation", JOptionPane.INFORMATION_MESSAGE);
         }
-
+    }
+    public static void firstoder_twovars(){
         // System of first order equation with 2 variables
 
         double a11, a12, a21, a22, b1, b2;
@@ -42,14 +43,15 @@ public class Equation{
             double x2 = (a11 * b2 - a21 * b1) / det;
             JOptionPane.showMessageDialog(null, "Solution: x1 = " + x1 + ", x2 = " + x2, "Solution of system of equations", JOptionPane.INFORMATION_MESSAGE);
         }
-
+    }   
+    public static void secondorder_onevar(){
         // second order equation with 1 variable 
         String stra, strb, strc; 
-        stra = Strnum1 = JOptionPane.showInputDialog(null, "Please Input a", "Second order equation with one variable", JOptionPane.INFORMATION_MESSAGE);
+        stra = JOptionPane.showInputDialog(null, "Please Input a", "Second order equation with one variable", JOptionPane.INFORMATION_MESSAGE);
         double a = Double.parseDouble(stra);
-        strb = Strnum1 = JOptionPane.showInputDialog(null, "Please Input b", "Second order equation with one variable", JOptionPane.INFORMATION_MESSAGE);
+        strb = JOptionPane.showInputDialog(null, "Please Input b", "Second order equation with one variable", JOptionPane.INFORMATION_MESSAGE);
         double b = Double.parseDouble(strb);
-        strc = Strnum1 = JOptionPane.showInputDialog(null, "Please Input c", "Second order equation with one variable", JOptionPane.INFORMATION_MESSAGE);
+        strc = JOptionPane.showInputDialog(null, "Please Input c", "Second order equation with one variable", JOptionPane.INFORMATION_MESSAGE);
         double c = Double.parseDouble(strc);
         if(a == 0) JOptionPane.showMessageDialog(null, "Error value of a", "Error", JOptionPane.INFORMATION_MESSAGE);
         else{
@@ -65,5 +67,19 @@ public class Equation{
                 JOptionPane.showMessageDialog(null, "x1 = " + x + ", x2 = " + y, "Roots of equation", JOptionPane.INFORMATION_MESSAGE);
             } 
         }
+    }
+    public static void main(String args[]){
+        String a[]={"1","2"};
+        int numvar=   Integer.parseInt((String)JOptionPane.showInputDialog(null,"Input the number of variables:","Number of variables of equation",
+        JOptionPane.QUESTION_MESSAGE,null,a,1));
+        if(numvar == 1){
+            int degree=   Integer.parseInt((String)JOptionPane.showInputDialog(null,"Input the degree of the equation:","Degree of equation",
+            JOptionPane.QUESTION_MESSAGE,null,a,1));
+            if(degree == 1 ){
+                Equation.firstoder_onevar(); 
+            }
+            else Equation.firstoder_twovars();
+        }
+        else Equation.secondorder_onevar();
     }
 }
